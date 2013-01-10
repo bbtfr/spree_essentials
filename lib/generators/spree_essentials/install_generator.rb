646f37d5aa82e6c11d68be51af3f67194abf9c98
LiYang <theo@secondbureau.com>
2013-01-10 15:41:36 +0800

    fix the generators

--- lib/generators/spree_essentials/install_generator.rb
+++ lib/generators/spree_essentials/install_generator.rb
@@ -2,21 +2,13 @@ module SpreeEssentials
   module Generators
     class InstallGenerator < Rails::Generators::Base
       
-      desc "Installs required migrations for spree_essentials"
-      
-      class_option :add_stylesheets, :type => :boolean, :default => true, :banner => 'Append spree_essentials to admin/all.css'
-      class_option :add_javascripts, :type => :boolean, :default => true, :banner => 'Append spree_essentials to admin/all.js'
-      
-      def append_stylesheets
-        return unless options[:add_stylesheets]
-        gsub_file "app/assets/stylesheets/admin/all.css", "*/", "*= require admin/spree_essentials\n*/"  
-      end
-      
-      def append_javascripts
-        return unless options[:add_javascripts]
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_essentials"
+      def add_javascripts
+        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_essentials\n"
       end
 
+      def add_stylesheets
+        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_essentials\n", :before => /\*\//, :verbose => true
+      end
     end
   end
 end
