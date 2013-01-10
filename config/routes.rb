e930a71a3c5046b52498b82e8f7a1211f7644241
LiYang <theo@secondbureau.com>
2013-01-10 15:43:13 +0800

    remove uploads support, use spree_editor instead

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
     post "/markdown/preview" => "markdown#preview", :as => :markdown_preview
-    resources :uploads
-    resources :ckeditor_uploads
   end
 end
